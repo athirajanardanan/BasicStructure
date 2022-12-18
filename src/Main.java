ffc14c4d53ce877854198682ac29f18a2c3efa52
athirajanardanan <athi.j89@gmail.com>
2022-12-18 11:20:03 -0500

    The Basic Project Solar System is ready for the review

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is just a basic structure");
+   solarSytem solar=new solarSystem();
+
+   
     }
 }
